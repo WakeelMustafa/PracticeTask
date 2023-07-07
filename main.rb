ba090b1a87f721d8c7603c4e306b4ac0ed5a3d93
WakeelMustafa <wakeelmustafa999@gmail.com>
2023-07-07 16:55:27 +0500

    Code Refactor

--- main.rb
+++ main.rb
@@ -1,5 +1,8 @@
+require( './task_1' )
 require( './task_2' )
 begin
+object = Task1.new
+puts object.frequency_count([1, 2, 3, 4, 1, 1, 1, 2, 3, 11, 11, 2, 33])
 object = Task2.new
 puts 'Running Sum'
 object.array_sum
